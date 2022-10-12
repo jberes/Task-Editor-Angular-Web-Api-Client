9adcab237853ea3bdd92898527e175b1402c97a5
Jason Beres <jasonberes@gmail.com>
2022-10-12 09:44:06 -0400

    Removed test code, made public

--- src/app/models/tasks.model.ts
+++ src/app/models/tasks.model.ts
@@ -8,15 +8,3 @@ projectId = 0;
 assignedToEmail = '';
 priority = 0;
 }
-
-
-export class TaskNN {
-    taskID = 0;
-    taskName = '';
-    dateAdded = new Date();
-    dateUpdated = new Date();
-    dateDue = '';
-    projectID = 0;
-    assignedToEmail = '';
-    priority = 0;
-    }
--- src/app/services/tasks-web-api.service.ts
+++ src/app/services/tasks-web-api.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { firstValueFrom, Observable } from 'rxjs';
 import { Task } from '../models/tasks.model';
-import { TaskNN } from '../models/tasks.model';
 
 const API_ENDPOINT = 'https://localhost:7021/api';
 
